88284709379cfc086ab669ce9069fed334c30103
eduardoxoq <30791489+eduardoxoq@users.noreply.github.com>
2019-08-22 01:19:03 -0600

    comentario

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world1" << endl;
+
     cout << "Hello world2!" << endl;
     cout << "Hello world3!" << endl;
     cout << "Hello world4!" << endl;
