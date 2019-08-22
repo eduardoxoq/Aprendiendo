5eca14a036cf68be8321af40ded2473e564bda23
eduardoxoq <30791489+eduardoxoq@users.noreply.github.com>
2019-08-21 19:00:42 -0600

    ahora tiene mas HOlas MUndo

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Hello world1" << endl
+    cout << "Hello world2!" << endl;
+    cout << "Hello world3!" << endl;
+    cout << "Hello world4!" << endl;
     return 0;
 }
